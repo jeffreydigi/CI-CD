7aa226bfa7673c27c3c915c2da60e19d3ea5e302
jeffreydigi <jeffrey@digibloomsolutions.com>
2020-08-05 00:13:18 +0530

    Adding Simple React App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>DSPL - CI/CD Test Application</code>
+        </p>
+        <span>
+          <code>Using ReactJS</code>
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
